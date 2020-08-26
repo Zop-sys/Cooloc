58400a3647148d544b205646959838f3816813e5
Zoe Pralus <zoe.pralus@audencia.com>
2020-08-26 11:57:42 +0200

    creation form + controller op pour base

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,23 @@
 class TeamsController < ApplicationController
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Painting.new(team_params)
+
+      #attendre Mélanie pour l'envoie de mail
+
+    if @team.save
+      redirect_to @new_team_task, notice: 'Ta coloc a bien été crée ! '
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def team_param
+    params.require(:team).permit(:name)
+  end
 end
