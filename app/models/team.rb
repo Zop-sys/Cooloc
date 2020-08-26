4907128c9d22cbb0dc1df862a6775f843c1dbe95
Zoe Pralus <zoe.pralus@audencia.com>
2020-08-26 12:23:01 +0200

    ajout de team nill

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,10 @@
 class Team < ApplicationRecord
   has_many :users
   validates :name, presence: true, uniqueness: true
+
+    after_initialize :team_value
+
+  def team_value
+    self.team = nil
+  end
 end
