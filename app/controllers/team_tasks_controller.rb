ea8cc0f4e92015a9e89fa9a3bc9a5f1bcfc5fb6f
Zoe Pralus <zoe.pralus@audencia.com>
2020-08-27 18:17:59 +0200

    controller op

--- app/controllers/team_tasks_controller.rb
+++ app/controllers/team_tasks_controller.rb
@@ -4,12 +4,18 @@ class TeamTasksController < ApplicationController
   end
 
   def create
-    @team_task = TeamTask.new
-    @team_task.team = Current_user.team
-    if @team_task.save
-      redirect_to @team_chores, notice: 'Tes tâches sont bien enregistrées ! '
+    @team = current_user.team
+    @team.task_ids = team_param
+    if @team.save
+      redirect_to chores_path, notice: 'Tes tâches sont bien enregistrées ! '
     else
       render :new
     end
+  end
+
+  private
+
+  def team_param
+    params.require(:team_task).permit(:task_id)
   end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,7 @@
 class Team < ApplicationRecord
   has_many :users
   validates :name, presence: true, uniqueness: true
+
+  has_many :team_tasks
+  has_many :tasks, through: :team_tasks
 end
