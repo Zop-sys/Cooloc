1ef85bd133307c9ada45a0978a67074df9069262
Isabelle Pouget <isab.pouget@gmail.com>
2020-08-27 16:32:01 +0200

    Continue configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Cooloc_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method     = :postmark
+  config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
+  config.action_mailer.default_url_options = { host: "www.cooloc.site" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
