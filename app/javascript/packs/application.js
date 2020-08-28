9848d3711b65494d9515b005b075646ecc21b02a
Isabelle Pouget <isab.pouget@gmail.com>
2020-08-28 12:38:42 +0200

    Finish JS add a coloc

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,8 +27,11 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+// Add coloc
+import { initAddRoomateEmailInput } from '../plugins/teams/new';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initAddRoomateEmailInput();
 });
