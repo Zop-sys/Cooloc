b63d387f120aed086c6d2e70d89ec498a9cc5a4b
Zoe Pralus <zoe.pralus@audencia.com>
2020-09-01 13:02:33 +0200

    Controller / view pour la team

--- app/controllers/team/chores_controller.rb
+++ app/controllers/team/chores_controller.rb
@@ -1,7 +1,31 @@
 class Team
   class ChoresController < ApplicationController
     def index
-      # lister les chores de la team du current_user
+      @chores                       = current_user.team.chores.includes(:task)
+      @monthly_chores               = @chores.where(tasks: { frequency: 'monthly' })
+      @weekly_chores                = @chores.where(tasks: { frequency: 'weekly' })
+      @weekly_chores_by_week_number = @chores.where(tasks: { frequency: 'weekly' }).group_by(&:week_number)
+    end
+
+    def change_status_done
+      @chore = Chore.find(params[:id])
+      @chore.status = "done"
+      @chore.save
+      redirect_to chores_path
+    end
+
+    def change_status_uncomplete
+      @chore = Chore.find(params[:id])
+      @chore.status = "uncomplete"
+      @chore.save
+      redirect_to chores_path
+    end
+
+    def change_status_pending
+      @chore = Chore.find(params[:id])
+      @chore.status = "pending"
+      @chore.save
+      redirect_to chores_path
     end
   end
 end
