5d5bd98d8b2696378499ac3609f8da8492f75f95
Zoe Pralus <zoe.pralus@audencia.com>
2020-09-01 13:19:11 +0200

    in prpgress

--- app/controllers/team/chores_controller.rb
+++ app/controllers/team/chores_controller.rb
@@ -1,10 +1,13 @@
 class Team
   class ChoresController < ApplicationController
     def index
-      @chores                       = current_user.team.chores.includes(:task)
+      @team                        = current_user.team
+      @chores_users                = Chore.all.where(users: { team: @team })
+      @chores                       = @chores_users.includes(:task)
       @monthly_chores               = @chores.where(tasks: { frequency: 'monthly' })
       @weekly_chores                = @chores.where(tasks: { frequency: 'weekly' })
-      @weekly_chores_by_week_number = @chores.where(tasks: { frequency: 'weekly' }).group_by(&:week_number)
+      @weekly_chores_by_week_number = @chores.where(tasks: { frequency: 'weekly' })
+                                            .group_by(&:week_number)
     end
 
     def change_status_done
